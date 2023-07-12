2d4c2bf559a3184092efacd23237d4d6ae25c63e
Shashank datta <68425706+shashankdatta@users.noreply.github.com>
2023-07-12 08:20:39 -0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,6 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </head>
 
