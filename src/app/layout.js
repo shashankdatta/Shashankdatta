2ebfbd272a0f1c1e2874c387b865c9c33c44ba36
Shashank datta Bezgam <shashankdatta@vt.edu>
2023-07-12 23:25:39 +0530

    Fixed Favicon Problem

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </head>
 
