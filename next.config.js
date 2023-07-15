fae6ba3eacf89edc8c35bfe2c2ed86290ce25a81
Shashank datta Bezgam <shashankdatta@vt.edu>
2023-07-16 01:45:09 +0530

    final changes

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "/Shashankdatta",
+  basePath: "/Portfolio",
 
   /**
    * Disable server-based image optimization. Next.js does not support
